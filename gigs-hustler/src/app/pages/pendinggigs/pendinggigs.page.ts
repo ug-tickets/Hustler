e89af4198296ab90199511757079132b8561fa42
kdenno <kabuyedennis3@gmail.com>
2020-01-09 17:19:35 +0300

    create pending gigs interfaces

--- gigs-hustler/src/app/pages/pendinggigs/pendinggigs.page.ts
+++ gigs-hustler/src/app/pages/pendinggigs/pendinggigs.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { SegmentChangeEventDetail } from '@ionic/core';
 
 @Component({
   selector: 'app-pendinggigs',
@@ -7,9 +9,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PendinggigsPage implements OnInit {
 
-  constructor() { }
+  RecentHustlers = false;
+
+  constructor(public router: Router) { }
 
   ngOnInit() {
+  }
+  toGigDetails() {
+    this.router.navigateByUrl('/gigdetails');
+  }
+  toReviews() {
+    this.router.navigateByUrl('/gigreviews');
+  }
+  onFilter(event: CustomEvent<SegmentChangeEventDetail>) {
+    if (event.detail.value === 'recenthustlers') {
+      this.RecentHustlers = true;
+
+    } else {
+      this.RecentHustlers = false;
+
+    }
+
   }
 
 }
